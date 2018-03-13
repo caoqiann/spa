3b951f92bd8800f91f48a06ac0f2623bb9c88449
caaoqian <956275479@qq.com>
2018-03-13 16:36:01 +0800

    update

---
+++ second/time.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    
+
+  var val = document.getElementById('btn');
+  var  i = 10;
+  var setid;
+
+  function time(){
+    if(i!=0){
+      i--;
+      val.value="同意("+i+"s)";
+    }
+    else{
+      val.value="同意";
+      val.disabled=false;
+      clearInterval(setid);
+    }
+  }
+  setid=setInterval(function(){time()},1000);
+
+
+};
